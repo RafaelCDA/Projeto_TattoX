1dd6fd2e3ab1a0c39bb6cdb994f2ab102b5a0766
Renan Costa <renancs.sousa@gmail.com>
2024-06-21 11:45:09 -0300

    alteracoes view

--- src/routes/perfilRouter.js
+++ src/routes/perfilRouter.js
@@ -6,7 +6,7 @@ const perfilController = require('../controller/perfilController');
 router.get('/', perfilController.indexView);
 router.get('/login', perfilController.loginView);
 router.get('/cadastro', perfilController.cadastroView);
-router.get('/banco', perfilController.bancoView); // Nova rota para visualizar o banco
+router.get('/banco', perfilController.bancoView);
 
 router.post('/cadastro', perfilController.handleCadastro);
 router.post('/login', perfilController.handleLogin);
